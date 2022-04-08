bff75a91d5e65bf796f5190cd53f868384517991
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2022-04-08 11:05:29 +0300

    matchers

---
+++ src/__tests__/health.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import healthLevel from '../players';
+
+test('sorting', () => {
+  const testing = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const result = healthLevel(testing);
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
